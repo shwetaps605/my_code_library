f179470ff42d8516d6834f84e1665e5e1c9e1952
SHWEROY_T3DT <shweroy@apac.corpdir.net>
2024-08-25 12:02:37 +0530

    merge sort done

--- sorting-algorithms/merge-sort.js
+++ sorting-algorithms/merge-sort.js
@@ -1,7 +1,22 @@
 
+function mergeArraysVersion2 (left,right) {
+    let sortedArray = [];
+    while(left?.length && right?.length) {
+        if(left[0] < right[0]) {
+            console.log('left is smaller so after shift left-->',left)
+            sortedArray.push(left.shift())
+        } else {
+            console.log('right is smaller so after shift right-->',right)
+            sortedArray.push(right.shift())
+        }
+    }
+    return [...sortedArray,...left,...right]
+}
+
+
 function mergeArrays(arr1,arr2,array) {
-    const l1 = arr1.length;
-    const l2 = arr2.length;
+    const l1 = arr1?.length;
+    const l2 = arr2?.length;
     let i = 0;
     let j = 0;
     let k = 0;
@@ -39,27 +54,30 @@ function mergeSort(array) {
     const mid = Math.floor(n/2);
     console.log("MID VAL-->",mid)
 
-    let arrayOne = new Array(mid);
-    let arrayTwo = new Array(n-mid);
+    let arrayOne = array.slice(0,mid);
+    let arrayTwo = array.slice(mid);
 
-    
-
-    for(i=0;i<mid-1;i++)
-        console.log(`i=${i}`)
-        arrayOne[i] = array[i];
+    // for(i=0;i<mid-1;i++)
+    //     console.log(`i=${i}`)
+    //     arrayOne[i] = array[i];
     console.log("ARRAY ONE", arrayOne)
-    for(j=mid;j<n-1;j++)
-        console.log(`j=${j}`)
-        arrayTwo[j-mid] = array[j];
+    // for(j=mid;j<n-1;j++)
+    //     console.log(`j=${j}`)
+    //     arrayTwo[j-mid] = array[j];
     console.log("ARRAY TWO", arrayTwo)
 
 
-    mergeSort(arrayOne);
-    mergeSort(arrayTwo);
-    mergeArrays(arrayOne,arrayTwo, array)
+    let left = mergeSort(arrayOne);
+    let right = mergeSort(arrayTwo);
+    const sorted = mergeArraysVersion2(left,right)
+    return sorted;
 }
 
 
-let unsortedArray = [2,8,4,3,1,5]
-mergeSort(unsortedArray);
-console.log("SORTED",unsortedArray)
+let unsortedArray = [66,34,12,54,6,45,99,25,10,2,0,38]
+const sortedArray = mergeSort(unsortedArray)
+console.log("SORTED",sortedArray);
+
+
+//console.log(mergeArraysVersion2([1, 4], [2, 6, 9]));
+
