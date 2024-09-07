af4d46efc084b46bb5042d17c68bb442f44d612e
SHWEROY_T3DT <shweroy@apac.corpdir.net>
2024-09-07 22:53:53 +0530

    Added some recursion questions

--- data-structures-java/src/Main.java
+++ data-structures-java/src/Main.java
@@ -1,28 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         //Single Linked List
-//        CustomLinkedList linkedList = new CustomLinkedList();
-//        linkedList.insertFirst(5);
-//        linkedList.insertFirst(20);
-//        linkedList.insertFirst(7);
-//        linkedList.display();
-//        linkedList.insertEnd(19);
-//        linkedList.insertEnd(2);
-//        linkedList.display();
-//        linkedList.insertEndWithoutTail(28);
-//        linkedList.display();
-//        linkedList.insertAtNthPosition(9,3);
-//        linkedList.display();
-//        linkedList.insertAtNthPosition(24,2);
-//        linkedList.display();
-//        int removed = linkedList.deleteFirst();
-//        System.out.println(removed + " was removed");
-//        linkedList.insertFirst(12);
-//        linkedList.display();
-//        System.out.println(linkedList.deleteLast() + " was removed");
-//        linkedList.display();
-//        System.out.println(linkedList.deleteFromNthPosition(4) + " was removed");
-//        linkedList.display();
+        CustomLinkedList linkedList = new CustomLinkedList();
+        linkedList.insertFirst(5);
+        linkedList.insertFirst(20);
+        linkedList.insertFirst(7);
+        linkedList.display();
+        linkedList.insertEnd(19);
+        linkedList.insertEnd(2);
+        linkedList.display();
+        linkedList.insertEndWithoutTail(28);
+        linkedList.display();
+        linkedList.insertAtNthPosition(9,3);
+        linkedList.display();
+        linkedList.insertAtNthPosition(24,2);
+        linkedList.display();
+        int removed = linkedList.deleteFirst();
+        System.out.println(removed + " was removed");
+        linkedList.insertFirst(12);
+        linkedList.display();
+        System.out.println(linkedList.deleteLast() + " was removed");
+        linkedList.display();
+        System.out.println(linkedList.deleteFromNthPosition(4) + " was removed");
+        linkedList.display();
 
         //Double Linked List
         DoubleLinkedList dll = new DoubleLinkedList();
